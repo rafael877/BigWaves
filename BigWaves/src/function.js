5014a22eb2988935fb056e7d810219872c9fb9c9
Gustavo Souza <gustavo060508@gmail.com>
2025-05-29 16:05:01 -0300

    Adiciona logo e ajusta layout das páginas

--- BigWaves/src/function.js
+++ BigWaves/src/function.js
@@ -1,19 +1,49 @@
-  const slides = document.querySelectorAll('.marca');
-  const prevBtn = document.querySelector('.prev');
-  const nextBtn = document.querySelector('.next');
-  let currentIndex = 0;
+(function () {
+  const slidesMarcas = document.querySelectorAll(".marca");
+  const prevBtnMarcas = document.querySelector(".prevMarcas");
+  const nextBtnMarcas = document.querySelector(".nextMarcas");
+  let currentIndexMarcas = 0;
 
-  function showSlide(index) {
-    const slider = document.querySelector('.marcas');
+  function showSlideMarcas(index) {
+    const slider = document.querySelector(".marcas");
     slider.style.transform = `translateX(-${index * 100}%)`;
   }
 
-  prevBtn.addEventListener('click', () => {
-    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-    showSlide(currentIndex);
+  prevBtnMarcas.addEventListener("click", () => {
+    currentIndexMarcas =
+      (currentIndexMarcas - 1 + slidesMarcas.length) % slidesMarcas.length;
+    showSlideMarcas(currentIndexMarcas);
   });
 
-  nextBtn.addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % slides.length;
-    showSlide(currentIndex);
+  nextBtnMarcas.addEventListener("click", () => {
+    currentIndexMarcas = (currentIndexMarcas + 1) % slidesMarcas.length;
+    showSlideMarcas(currentIndexMarcas);
   });
+})();
+
+//--------------------------------------------------------------
+
+(function () {
+  const slidesAvaliacoes = document.querySelectorAll(".avaliacao");
+  const prevBtnAvaliacoes = document.querySelector(".prevAvaliacoes");
+  const nextBtnAvaliacoes = document.querySelector(".nextAvaliacoes");
+  let currentIndexAvaliacoes = 0;
+
+  function showSlideAvaliacoes(index) {
+    const slider = document.querySelector(".avaliacoes");
+    slider.style.transform = `translateX(-${index * 100}%)`;
+  }
+
+  prevBtnAvaliacoes.addEventListener("click", () => {
+    currentIndexAvaliacoes =
+      (currentIndexAvaliacoes - 1 + slidesAvaliacoes.length) %
+      slidesAvaliacoes.length;
+    showSlideAvaliacoes(currentIndexAvaliacoes);
+  });
+
+  nextBtnAvaliacoes.addEventListener("click", () => {
+    currentIndexAvaliacoes =
+      (currentIndexAvaliacoes + 1) % slidesAvaliacoes.length;
+    showSlideAvaliacoes(currentIndexAvaliacoes);
+  });
+})();
